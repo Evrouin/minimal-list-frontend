debd576da0482eb7c58e31b7937ed291533d4214
Evrouin <elwaycortez@gmail.com>
2026-03-15 12:46:18 +0800

    chore: upgrated nuxt to v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/eslint', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: [
+        'vue3-google-login',
+        '@tiptap/vue-3',
+        '@tiptap/starter-kit',
+        '@tiptap/extension-placeholder',
+      ],
+    },
   },
   ssr: false,
   routeRules: {
