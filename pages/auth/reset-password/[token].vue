442c492d6ea281aa966e278245ef3bc3a9d84b78
Evrouin <elwaycortez@gmail.com>
2026-04-03 11:38:39 +0800

    feat: add reset password validation

--- pages/auth/reset-password/[token].vue
+++ pages/auth/reset-password/[token].vue
@@ -16,6 +16,14 @@ const handleSubmit = async () => {
   errorMsg.value = ''
   successMsg.value = ''
 
+  if (form.new_password.length < 8) {
+    errorMsg.value = 'password must be at least 8 characters'
+    return
+  }
+  if (!/(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z0-9])/.test(form.new_password)) {
+    errorMsg.value = 'password must include letters, numbers, and a special character'
+    return
+  }
   if (form.new_password !== form.new_password2) {
     errorMsg.value = "passwords don't match"
     return
