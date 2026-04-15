85d60ae519ad97bf676e3e8e5cb95fe2c07b9a31
Evrouin <elwaycortez@gmail.com>
2026-04-15 23:17:12 +0800

    refactor: use at instead of backHandlers

--- composables/useBackHandler.ts
+++ composables/useBackHandler.ts
@@ -4,7 +4,7 @@ export const useBackHandler = () => {
   const push = (handler: () => void) => _backHandlers.value.push(handler)
   const pop = () => _backHandlers.value.pop()
   const handle = () => {
-    const top = _backHandlers.value[_backHandlers.value.length - 1]
+    const top = _backHandlers.value.at(-1)
     if (top) { top(); return true }
     return false
   }
